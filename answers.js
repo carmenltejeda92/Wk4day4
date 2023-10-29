5f3591ca035fc6fd764acf226f5ebdd13cc791c3
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-10-29 12:39:53 -0400

    Get Even answered

---
+++ answers.js
@@ -0,0 +1,107 @@
+/*  Author: C. Tejeda
+    Date: 10.27.2023
+    Purpose: Week 4/Day3 Lecture - Arrays, Loops, and Conditional Reps
+             Learning Objectives: -Practice with Arrays -Practice with Loops -Practice with Conditionals */
+
+
+/* Review - Part 1: HTML & CSS */
+/* HTML Intro (tags, content, attributes, semantic HTML) 
+    HTML(Hypertext Markup Language) is the backbone of a web page - browser will read it in a  text file & interpret it
+    Tags (<tag>(opening)</tag>(closing)) have content between the tags that serves another function 
+        - tells the browser how the page needs to be displayed
+        - all html documents must have the <html></html> tags
+        - all html documents must have the <body></body> tag
+            - this is where the main content of the website will be (i.e., like the body of an emial)
+        - disadvantage is the tags are verbose, requiring both an opening and closing tag
+    Attributes are what appear w/in the tags
+    Semantic html refers to choosing tags, not for how they display but for what function they perform
+*/
+
+/* HTML Elements (headers, navigation, lists, sections, etc.) and Media (images, audio, videos)
+    <header></header> (header tag) usually the company's logo - the first tag w/in the body - <h1> goes here usually
+    <footer></footer> (footer tag) displays at the bottom of the web page - last tag w/in the body 
+    <main></main> (main tag) is the main content of the web page - ie, the search engine for google 
+        -goes after the header, w/in the body
+        -holds content such as <p> tags and <h2>, <a>, etc.
+    <nav></nav> (navigation tag) usually seen at the top of the document w/in the <header> tag for the navigation through the website (navigation bar)
+        -tells document there's navigation but the links still need put in
+        -different from hyperlinks
+    <aside></aside> - used for adds a lot - related to the article sometimes
+    <article></article> content
+    <section></section> is to dileniate sections 
+    <div></di> & <span></span> are placeholders of sorts - divs used a lot before HTML 5 
+        -span is used for something that needs set apart but not need it's own section
+    To include images, audio, & video:
+        <img src /> is used to display images - self closing tag
+        <audio src></audio> is used to play audio
+            -this tag includes a controls attribute
+            -includes an autoplay option
+        <video></video> is used to display video
+            -also includes controls
+            -also includes autopay 
+*/
+
+/* CSS Intro (including css files in HTML, elements)
+    CSS is used to make the website look pretty
+    CSS stands for cascading style sheets - defines the style of the website following a set of rules
+        -to use CSS, a <head></head> (head tag) is needed w/in the <html> tag but outside the <body> tag
+            -this <head> tag is different than the <header> tag
+            -contains meta information about the page & is used to tell the browser how to use the document (like instructions)
+        <link /> is used to link the html document to the stylesheet - this tag is similar to an anchor tag
+    adds style to the html tags using a selector, such as h1 for the h1 tag in the html document
+        -a property it can have would be the color attribute to give the property a value - text color 
+*/
+
+/* Part II: JavaScript Reps
+Write a for loop that will log the numbers 1 through 20.*/ 
+// for(let i=1; i<=20; i++){
+//     console.log(i)
+// }
+
+/*Write a for loop that will log only the even numbers in 0 through 200.*/
+// for(let i=0; i<=200; i++){
+//     console.log(i++)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
