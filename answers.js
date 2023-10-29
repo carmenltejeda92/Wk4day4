c541a17644a68f2bd0ad25dd6fcf3e25180bc014
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-10-29 19:19:28 -0400

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -78,11 +78,11 @@ If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number. */
 /* Wild Wild Life
 Use the following arrays to answer the questions below (name,species ,age, hometown): 
 You should be modifying the elements by accessing them. It is up to you which methods to use. */
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
 //1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
 // plantee[2] = 5001
@@ -101,8 +101,13 @@ const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 // console.log(wolfy)
 
 
-
-
+/* Yell at the Ninja Turtles
+Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result. */
+// const turtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+// for(let x of turtles){
+//     console.log(x.toUpperCase())
+// }
 
 
 
