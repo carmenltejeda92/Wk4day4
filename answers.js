98ee09fdeb0d71eee6f6b5ec4793f49c55e8bc48
carmenltejeda92 <carmenltejeda92@gmail.com>
2023-10-29 19:11:46 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -75,15 +75,30 @@ If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number. */
 //     else    console.log(i)
 // }
 
+/* Wild Wild Life
+Use the following arrays to answer the questions below (name,species ,age, hometown): 
+You should be modifying the elements by accessing them. It is up to you which methods to use. */
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+//1. Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+// plantee[2] = 5001
+// console.log(plantee)
 
+//2. Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+// wolfy[3] = "Gotham City"
+// console.log(wolfy)
 
+//3. Give D'Art a second hometown by adding "Hawkins"
+// dart.push("Hawkins")
+// console.log(dart)
 
-
-
-
-
-
+//4. Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+// wolfy.splice(0, 1, "Gameboy")
+// console.log(wolfy)
 
 
 
